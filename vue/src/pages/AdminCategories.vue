088a4a1aa1b858235b0bb2738a4f4abe3d45dab7
Kamil Chmielewski <kamster94@users.noreply.github.com>
2021-09-22 00:06:36 +0200

    Add admin panel (#8)
    * Created template and routings for admin panel
    * Separated header and footer into their own components

---
+++ vue/src/pages/AdminCategories.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="justify-evenly q-pa-md items-start q-gutter-md items-center">
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AdminCategoriesPage',
+});
+</script>
--- vue/src/router/routes.ts
+++ vue/src/router/routes.ts
@@ -6,6 +6,46 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/Index.vue') }],
   },
+  {
+    path: '/admin',
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [{ path: '', component: () => import('src/pages/Admin.vue') }],
+  },
+  {
+    path: '/admin/articles',
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/AdminArticles.vue') },
+    ],
+  },
+  {
+    path: '/admin/categories',
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/AdminCategories.vue') },
+    ],
+  },
+  {
+    path: '/admin/tags',
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/AdminTags.vue') },
+    ],
+  },
+  {
+    path: '/admin/media',
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/AdminMedia.vue') },
+    ],
+  },
+  {
+    path: '/admin/users',
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/AdminUsers.vue') },
+    ],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
