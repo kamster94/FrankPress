5ac666e098d2075b46ceac2d1b3360dc8da73f86
Kamil Chmielewski <kamster94@users.noreply.github.com>
2021-09-23 00:28:40 +0200

    Added authentication on frontend

---
+++ vue/src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="!isAuthenticated && !loading">
+    <button id="qsLoginBtn" class="btn btn-primary btn-block" @click="login">
+      Log in
+    </button>
+  </div>
+</template>
+
+<script>
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { inject } from 'vue';
+export default {
+  name: 'AppHeader',
+  inject: ['Auth'],
+  methods: {
+    login() {
+      this.Auth.loginWithRedirect();
+    },
+    logout() {
+      this.Auth.logout();
+      this.$router.push({ path: '/' });
+    }
+  },
+  setup() {
+    const auth = inject('Auth');
+    return {
+      ...auth
+    };
+  }
+};
+</script>
+
+<style>
+#mobileAuthNavBar {
+  min-height: 125px;
+  justify-content: space-between;
+}
+</style>
--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,6 +1,25 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from '@/App.vue';
+import { Auth0 } from '@/auth';
+import router from '@/router';
+import store from '@/store';
 
-createApp(App).use(store).use(router).mount("#app");
+async function init() {
+  const AuthPlugin = await Auth0.init({
+    onRedirectCallback: (appState) => {
+      router.push(
+        appState && appState.targetUrl
+          ? appState.targetUrl
+          : window.location.pathname
+      );
+    },
+    clientId: process.env.VUE_APP_AUTH0_CLIENT_KEY,
+    domain: process.env.VUE_APP_AUTH0_DOMAIN,
+    audience: process.env.VUE_APP_AUTH0_AUDIENCE,
+    redirectUri: window.location.origin
+  });
+  const app = createApp(App);
+  app.use(AuthPlugin).use(store).use(router).mount('#app');
+}
+
+init();
